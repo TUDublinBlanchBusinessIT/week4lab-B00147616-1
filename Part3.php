75de3a10f6f12fb4e64b440e31a133266943cd7d
B00147616 Dilnaz Iskandarova <B00147616@mytudublin.ie>
2023-10-11 11:00:51 +0100

    added aql

---
+++ Part3.php
@@ -0,0 +1,53 @@
+<?php
+//Give the name of the program here
+//Include your name and the date here
+//Give a brief description of what the program does
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "Tennisclub";
+$port = 3307;
+
+//set the default timezone - this is necessary since MySQL 8. This is an effort to store all dates and times together with their timezones. 
+//This is particularly important where there is a timestamp indicating when something happened.
+date_default_timezone_set('Europe/Dublin');
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname, $port);
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "myDB";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "SELECT id, firstname, lastname FROM MyGuests";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+  // output data of each row
+  while($row = mysqli_fetch_assoc($result)) {
+    echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+
+mysqli_close($conn);
+?> member (firstname, surname) VALUES ('john', 'doe')";
+
+mysqli_query($conn, $sql);
+
+mysqli_close($conn);
+?>
